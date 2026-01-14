34e5f24f439fe6a9bb5572aed75b239220f610d8
rlxlxl <slavavik1337@mail.ru>
2026-01-15 01:07:31 +0700

    adding gets/sets

--- class/users.hpp
+++ class/users.hpp
@@ -5,12 +5,71 @@
 
 class User {
 protected:
-    int password;
-    string firstName;
-    string lastName;
-    string email;
+    string password = "default";
+    string firstName = "default";
+    string lastName = "default";
+    string email = "default";
 
 public:
-    User (int pass, string fn, string ln, string em) :
+    User (string pass, string fn, string ln, string em) :
         password(pass), firstName(fn), lastName(ln), email(em) {}
+
+    string getPassword() {
+        return password;
+    }
+
+    string getFirstName() {
+        return firstName;
+    }
+
+    string getLastName() {
+        return lastName;
+    }
+
+    string getEmail() {
+        return email;
+    }
+
+    void setPassword() {
+        string oldPassword, newPassword;
+        cout << "Введите старый пароль: ";
+        cin >> oldPassword;
+
+        if (getPassword() == oldPassword) {
+            cout << "Введите новый пароль: ";
+            cin >> newPassword;
+            password = newPassword;
+            cout << "Пароль успешно изменен" << endl;
+        } else {
+            cout << "Неверный пароль" << endl;
+        }
+        return;
+    }
+
+    void setFirstName() {
+        string newFirstName;
+        cout << "Введите новое имя: ";
+        cin >> newFirstName;
+        firstName = newFirstName;
+        cout << "Имя успешно изменено" << endl;
+        return;
+    }
+    
+    void setLastName() {
+        string newLastName;
+        cout << "Введите новую фамилию: ";
+        cin >> newLastName;
+        lastName = newLastName;
+        cout << "Фамилия успешно изменена" << endl;
+        return;
+    }
+    
+    void setEmail() {
+        string newEmail;
+        cout << "Введите новый email: ";
+        cin >> newEmail;
+        email = newEmail;
+        cout << "Email успешно изменен" << endl;
+        return;
+    }
 };
